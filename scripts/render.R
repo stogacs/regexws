bf63ee9f40beb6b24d404711161181db3786dc4e
Eric Zhao <21zhaoe@protonmail.com>
2020-04-22 17:58:57 -0400

    Manually source local Rprofile in render script

--- scripts/render.R
+++ scripts/render.R
@@ -1,4 +1,10 @@
 #!/usr/bin/env Rscript
+initial_options <- commandArgs(trailingOnly = FALSE)
+scriptdir <- dirname(sub("--file=", "", initial_options[grep("--file=", initial_options)]))
+
+setwd(file.path(scriptdir, ".."))
+source(".Rprofile")
+
 tmpdir <- Sys.getenv("TMP_DIR")
 distdir <- Sys.getenv("DIST_DIR")
 rmarkdown::render(file.path(tmpdir, "index.Rmd"),
